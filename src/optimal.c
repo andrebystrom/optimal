d47f05c28119d136824df5eb99d644ca25df7ae1
André Byström <c20abm@cs.umu.se>
2024-02-08 14:53:54 +0100

    reworked API into builder API. Started sketching layout and functionality of it.

--- src/optimal.c
+++ src/optimal.c
@@ -1,13 +1,53 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "optimal.h"
 
-int optimal_run(struct optimal_command *commands, int argc, char **argv)
+static struct optimal_builder int_optimal_builder;
+static struct optimal_builder *const p_builder = &int_optimal_builder;
+
+struct optimal_builder *optimal_builder(void)
 {
-    optimal_command_handler handler;
-    struct optimal_command command;
+    int_optimal_builder.build = build;
+    int_optimal_builder.add_command = add_command;
+    int_optimal_builder.num_commands = 0;
+    return &int_optimal_builder;
+}
+
+int build(void)
+{
+}
+
+struct optimal_command_builder *add_command(char *name)
+{
+    struct optimal_command_builder *current;
+    current = p_builder->commands + p_builder->num_commands++;
+    strncpy(current->command_name, name, OPTIMAL_MAX_COMMAND_NAME);
+    current->command_name[OPTIMAL_MAX_COMMAND_NAME] = '\0';
+    current->num_args = 0;
+    return current;
+}
+
+struct optimal_command_builder *add_arg(char short_name, char *long_name,
+                                        enum optimal_value take_value,
+                                        enum optimal_type value_type)
+{
+    struct optimal_command_builder *current;
+    struct optimal_arg *current_arg;
+
+    current = p_builder->commands + p_builder->num_commands - 1;
+    current_arg = current->args + current->num_args++;
+
+    current_arg->short_name[0] = short_name;
+
+    current_arg->short_name[1] = '\0';
+    strncpy(current_arg->long_name, long_name, OPTIMAL_MAX_ARG);
+    current_arg->long_name[OPTIMAL_MAX_ARG] = '\0';
     
-    return handler(&command, argc, argv);
+    current_arg->take_value = take_value;
+    current_arg->type = value_type;
+
+    return current;
 }
--- src/optimal.h
+++ src/optimal.h
@@ -5,32 +5,75 @@
 #include <stdint.h>
 
 #define OPTIMAL_MAX_OPTS 20
+#define OPTIMAL_MAX_LONG_NAME 20
+#define OPTIMAL_MAX_ARG 20
+#define OPTIMAL_MAX_COMMAND_NAME 20
+#define OPTIMAL_MAX_COMMANDS 20
 
 #define OPTIMAL_MANDATORY 1
 #define OPTIMAL_HAS_VAL 2
 
-struct optimal_option;
-struct optimal_command;
-
-typedef int (*optimal_command_handler)(struct optimal_command *command,
-                                       int restc, char **restargv);
-
-struct optimal_option
+struct optimal_bucket
 {
-    char *long_name;   // Nullable
-    char short_name;   // 0 to not use
-    char *description; // Nullable
-    int flags;
-    char *value; // set by lib
+    char *key;
+    void *val;
 };
 
-struct optimal_command
+struct optimal_param_table
 {
-    char *command_name;                              // Nullable.
-    struct optimal_option options[OPTIMAL_MAX_OPTS]; // Null terminated
-    optimal_command_handler handler;
+    struct optimal_bucket buckets[OPTIMAL_MAX_OPTS * OPTIMAL_MAX_ARG];
 };
 
-int optimal_run(struct optimal_command *commands, int argc, char **argv);
+enum optimal_value
+{
+    OPTIMAL_OPTIONAL,
+    OPTIMAL_NONE,
+    OPTIMAL_REQUIRED
+};
+
+enum optimal_type
+{
+    OPTIMAL_INT,
+    OPTIMAL_FLOAT,
+    OPTIMAL_STRING
+};
+
+struct optimal_arg
+{
+    char short_name[2];
+    char long_name[OPTIMAL_MAX_LONG_NAME + 1];
+    enum optimal_value take_value;
+    enum optimal_type type;
+};
+
+struct optimal_command_builder
+{
+    int num_args;
+    struct optimal_arg args[OPTIMAL_MAX_ARG];
+    char command_name[OPTIMAL_MAX_COMMAND_NAME + 1];
+    int (*handler)(struct optimal_param_table *);
+
+    struct optimal_command_builder (*add_arg)(char, char *, enum optimal_value,
+                                              enum optimal_type);
+};
+
+struct optimal_builder
+{
+    int num_commands;
+    struct optimal_command_builder commands[OPTIMAL_MAX_COMMANDS];
+    struct optimal_command_builder (*add_command)(char *command);
+    int (*build)(void);
+};
+
+struct optimal_builder *optimal_builder(void);
+
+/* b->add_command("hej")
+    ->add_arg('n', "name", OPTIMAL_MANDATORY, OPTIMAL_VAL_STR)
+    ->add_arg('b', NULL, OPTIMAL_OPTIONAL, OPTIMAL_NO_VAL)
+    ->add_handler(my_func);
+b->build(argc, argv);
+b->cleanup();
+
+bool b = *(bool *)args->get("b"); */
 
 #endif
