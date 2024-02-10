f144da5413b34b8173543fdf4e09d34bd2575ef1
André Byström <c20abm@cs.umu.se>
2024-02-11 00:37:34 +0100

    added default handler to demo code

--- src/test.c
+++ src/test.c
@@ -25,6 +25,14 @@ int handle_del(struct optimal_param_table *params, int restc, char **restv)
     return 0;
 }
 
+int handle_default(struct optimal_param_table *params, int restc, char **restv)
+{
+    (void) restc;
+    (void) restv;
+    printf("default! %s\n", (char *)param_get(params, 'h', "help"));
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     struct optimal_builder *app = optimal_builder("add and delete things");
@@ -41,5 +49,10 @@ int main(int argc, char **argv)
         ->add_arg('n', "name", OPTIMAL_REQUIRED, OPTIMAL_STRING)
         ->add_arg('w', "why", OPTIMAL_OPTIONAL, OPTIMAL_FLAG)
         ->add_handler(handle_del);
+    
+    app->add_command(NULL)
+        ->add_arg('h', "help", OPTIMAL_OPTIONAL, OPTIMAL_STRING)
+        ->add_handler(handle_default);
+    
     return app->build(argc, argv);
 }
